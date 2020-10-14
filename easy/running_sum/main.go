f6855011a4131596b12d77d30b1fc3025e41352d
Adam Fadhil S <adam@icehousecorp.com>
2020-10-14 18:58:28 +0700

    Improved solution for running-sum

--- easy/running_sum/main.go
+++ easy/running_sum/main.go
@@ -7,6 +7,24 @@ import "fmt"
 func main() {
 	result := runningSum([]int{1, 2, 3})
 	fmt.Println(result)
+
+	result = runningSumV2([]int{1, 2, 3})
+	fmt.Println(result)
+}
+
+// From discussion.
+// Uses only 1 for loop.
+// What it does: Add total of n-1 + n
+func runningSumV2(nums []int) []int {
+	runningSum := []int{}
+	runningSum = append(runningSum, nums[0])
+
+	for i := 1; i < len(nums); i++ {
+		tempTotal := runningSum[i-1] + nums[i]
+		runningSum = append(runningSum, tempTotal)
+	}
+
+	return runningSum
 }
 
 func runningSum(nums []int) []int {
